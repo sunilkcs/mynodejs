87f434fa160c6f3e1322dc11f2198a3c6fcc03d1
sunil.cs <sunil.cs@nwmworld.com>
2019-12-16 17:38:30 +0530

    nodejsapp added with all capabilities

---
+++ nodejsapp/index.js
@@ -0,0 +1,9 @@
+exports.nodejshttpfunc = (req,res) => {
+  console.log("Response from my NodeJS function!! for http trigger");
+  res.status(200).send("Response from my NodeJS function!!");
+};
+
+exports.nodejspubsubfunc = (event, context) => {
+  const msg = event.data;
+  console.log("The message from mytopic is " + Buffer.from(msg, 'base64').toString() + " for pubsub trigger!");
+}
