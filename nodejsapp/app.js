3f89ff7e4f801cfda6373eecd8a123ba403c3495
sunil.cs <sunil.cs@nwmworld.com>
2019-12-18 17:56:51 +0530

    todays changes

--- nodejsapp/app.js
+++ nodejsapp/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const parser = require('body-parser');
 const app = express();
+app.use(parser.json());
 
 app.get('/', (req, res) => {
   console.log();
-  res.send(`Hello from NodeJS application8080`);
+  res.send(`Testing custom runtime on flexisble app engine with endpoints`);
+});
+
+app.post('/echo', (req, res) => {
+  res.status(200).json({message: req.body.message});
 });
 
 const port = process.env.PORT || 8080;
